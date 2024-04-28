a8c54f228a479c30610d2a45700dd8dc935ed076
Nethmi Jayakody <nethmi.19@cse.mrt.ac.lk>
2024-04-28 18:02:00 +0530

    step counter advancing but buggy on z axis

--- lib/models/step_counter.dart
+++ lib/models/step_counter.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:health_companion_app/contollers/daily_target_controller.dart';
 import 'package:health_companion_app/models/steps_notifer.dart';
@@ -8,14 +9,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class StepCounter {
   final double _zThreshold;
-  final double _xyThreshold;
+  final double _xyzThreshold;
   StreamSubscription<AccelerometerEvent>? _streamSubscription;
+  double _previousX = 0.0;
+  double _previousY = 0.0;
   double _previousZ = 0.0; // Added for peak detection
   final StepNotifier provider;
 
-  StepCounter({required this.provider, double zThreshold = 2.0, double xyThreshold = 0.5})
+  StepCounter({required this.provider, double zThreshold = 1.6, double xyzThreshold = 1.6})
       : _zThreshold = zThreshold,
-        _xyThreshold = xyThreshold;
+        _xyzThreshold = xyzThreshold;
 
   Future<void> startListening() async {
     if (_streamSubscription == null) {
@@ -28,10 +31,20 @@ class StepCounter {
     _streamSubscription = null;
   }
 
-  bool _isPeak(double value) {
+  bool _isPeak(double X, double Y, double Z) {
+    print('prev x,y,z: $_previousX, $_previousY, $_previousZ');
+    print('x,y,z : $X, $Y, $Z');
     // Simple peak detection based on change in Z-axis
-    bool isPeak = value.abs() > _zThreshold && value * _previousZ < 0;
-    _previousZ = value; // Update for next peak detection
+    double prevAbsXYZ = sqrt(_previousX*_previousX + _previousY*_previousY + _previousZ*_previousZ);
+    print('Prev abs $prevAbsXYZ');
+    double absXYZ =sqrt( X*X + Y*Y + Z*Z);
+    print('Abs: $absXYZ');
+    double value = prevAbsXYZ - absXYZ;
+    bool isPeak = value.abs() > _xyzThreshold && Z > 9.36;
+    isPeak? print('is Step : $isPeak'): print('');
+    _previousX = X;
+    _previousY = Y;
+    _previousZ = Z; // Update for next peak detection
     return isPeak;
   }
 
@@ -43,11 +56,9 @@ class StepCounter {
     double currentY = event.y.abs();
     int stepCount = prefs.getInt('counter')!;
     await prefs.setInt('counterP', stepCount);
-    if (_isPeak(currentZ)) {
-      if (currentX > _xyThreshold || currentY > _xyThreshold) {
+    if (_isPeak(currentX, currentY, currentZ)) {
         stepCount++;
         await prefs.setInt('counter', stepCount);
-      }
     }
     if(provider.steps == 0 && stepCount !=0){
       provider.addSteps(stepCount);
