8e2475e3e70f48e531214d6fcfc19cdaced8cd36
poornaSavindi <prnsavindi11@gmail.com>
2024-04-27 20:43:40 -0700

    Fix bugs

--- lib/screens/mood/mood_screen.dart
+++ lib/screens/mood/mood_screen.dart
@@ -21,7 +21,6 @@ import '../app_shell.dart';
 /// 6. surprise => 😲
 ///
 
-
 class MoodScreen extends StatefulWidget {
   final String formattedDate = DateFormat.yMMMMd().format(DateTime.now());
   final List<String>? dassScores;
@@ -34,7 +33,6 @@ class MoodScreen extends StatefulWidget {
 
 class _MoodScreenState extends State<MoodScreen>
     with SingleTickerProviderStateMixin {
-
   final TextEditingController _textController = TextEditingController();
   late final TabController _tabController;
   String name = '';
@@ -46,21 +44,29 @@ class _MoodScreenState extends State<MoodScreen>
   Future<User> getUser() async {
     User user = await UserController.getUser();
     if (user != null) {
-
       MoodRecord? todayRec = await EmotionsController.getTodayRecord(user.id!);
-      if(todayRec == null){
-        todayRec = MoodRecord(day: DateFormat('yyyy-MM-dd').format(DateTime.now()), sadness: 0, joy: 0, love: 0, anger: 0, fear: 0, surprise: 0, userId: user.id!);
+      if (todayRec == null) {
+        todayRec = MoodRecord(
+            day: DateFormat('yyyy-MM-dd').format(DateTime.now()),
+            sadness: 0,
+            joy: 0,
+            love: 0,
+            anger: 0,
+            fear: 0,
+            surprise: 0,
+            userId: user.id!);
         bool res = await EmotionsController.addRecord(todayRec);
-        if(res){
+        if (res) {
           todayRec = await EmotionsController.getTodayRecord(user.id!);
-          if(todayRec != null){
+          if (todayRec != null) {
             await getEmotions(user.id!);
           }
-        }else{
-          ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text('Something went wrong! Can not load previous records!')));
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(
+                  'Something went wrong! Can not load previous records!')));
         }
-      }else{
+      } else {
         await getEmotions(user.id!);
       }
 
@@ -73,8 +79,9 @@ class _MoodScreenState extends State<MoodScreen>
   }
 
   Future<Map<String, List<ChartData>>> getEmotions(int userId) async {
-    Map<String, List<ChartData>> emotions = await EmotionsController.getEmotions(userId);
-    if (emotions != null){
+    Map<String, List<ChartData>> emotions =
+        await EmotionsController.getEmotions(userId);
+    if (emotions != null) {
       this.weeklyData = emotions['weeklyData']!;
       this.monthlyData = emotions['monthlyData']!;
       this.yearlyData = emotions['yearlyData']!;
@@ -82,37 +89,45 @@ class _MoodScreenState extends State<MoodScreen>
     return emotions;
   }
 
-  void sendEmotion(String log) async{
+  void sendEmotion(String log) async {
     String? mood = await getMood(log);
-    if(mood != null && userId != null){
+    if (mood != null && userId != null) {
       MoodRecord? todayRec = await EmotionsController.getTodayRecord(userId!);
-      if(todayRec != null){
+      if (todayRec != null) {
         todayRec.increaseMood(mood);
-      }else{
-        todayRec = MoodRecord(day: DateFormat('yyyy-MM-dd').format(DateTime.now()), sadness: 0, joy: 0, love: 0, anger: 0, fear: 0, surprise: 0, userId: userId!);
+      } else {
+        todayRec = MoodRecord(
+            day: DateFormat('yyyy-MM-dd').format(DateTime.now()),
+            sadness: 0,
+            joy: 0,
+            love: 0,
+            anger: 0,
+            fear: 0,
+            surprise: 0,
+            userId: userId!);
         bool res = await EmotionsController.addRecord(todayRec);
-        if(!res){
-          ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text('Something went wrong! check your internet connection!')));
+        if (!res) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(
+                  'Something went wrong! check your internet connection!')));
           return;
-        }else{
+        } else {
           todayRec.increaseMood(mood);
         }
       }
       bool res = await EmotionsController.updateMoodRecord(todayRec);
       await getEmotions(userId!);
-      setState(() {
+      setState(() {});
 
-      });
-
-      if(!res){
-        ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('Something went wrong! check your internet connection!')));
+      if (!res) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content:
+                Text('Something went wrong! check your internet connection!')));
       }
-
-    }else{
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Something went wrong! check your internet connection!')));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content:
+              Text('Something went wrong! check your internet connection!')));
     }
   }
 
@@ -136,26 +151,43 @@ class _MoodScreenState extends State<MoodScreen>
         alignment: AlignmentDirectional.center,
         children: [
           Column(
-          children: [
-      Expanded(
-      flex: 2,
-          child: FutureBuilder(
-            future: userId == null ? Future.value(null) : getEmotions(userId!),
-            builder: (BuildContext context, AsyncSnapshot<Map<String, List<ChartData>>?> snapshot){
-              if (snapshot.connectionState == ConnectionState.waiting || snapshot.data == null) {
-                return Center(child: CircularProgressIndicator()); // Show a loading spinner while waiting
-              } else if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}'); // Show error message if any error occurred
-              } else {
-                return ChartSection(tabController: _tabController, weeklyData: weeklyData, monthlyData: monthlyData, yearlyData: yearlyData,);
-              }},
-          )),
-      Expanded(child: InputEmotion(textController: _textController, sendEmotion: sendEmotion)),
-      ],),
+            children: [
+              Expanded(
+                  flex: 2,
+                  child: FutureBuilder(
+                    future: userId == null
+                        ? Future.value(null)
+                        : getEmotions(userId!),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<Map<String, List<ChartData>>?> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting ||
+                          snapshot.data == null) {
+                        return Center(
+                            child:
+                                CircularProgressIndicator()); // Show a loading spinner while waiting
+                      } else if (snapshot.hasError) {
+                        return Text(
+                            'Error: ${snapshot.error}'); // Show error message if any error occurred
+                      } else {
+                        return ChartSection(
+                          tabController: _tabController,
+                          weeklyData: weeklyData,
+                          monthlyData: monthlyData,
+                          yearlyData: yearlyData,
+                        );
+                      }
+                    },
+                  )),
+              Expanded(
+                  child: InputEmotion(
+                      textController: _textController,
+                      sendEmotion: sendEmotion)),
+            ],
+          ),
           (widget.dassScores != null)
               ? AlertBox(scores: widget.dassScores!)
               : Container()
-          ],
+        ],
       ),
     );
   }
@@ -289,8 +321,8 @@ class _AlertBoxState extends State<AlertBox> {
                         context,
                         MaterialPageRoute(
                             builder: (context) => AppShell(
-                              currentIndex: 1,
-                            )),
+                                  currentIndex: 1,
+                                )),
                       );
                     },
                     child: Text(
@@ -310,10 +342,10 @@ class _AlertBoxState extends State<AlertBox> {
                         context,
                         MaterialPageRoute(
                             builder: (context) => AppShell(
-                              currentIndex: 2,
-                              healthTipsKeyWords:
-                              recommendedTips(widget.scores),
-                            )),
+                                  currentIndex: 2,
+                                  healthTipsKeyWords:
+                                      recommendedTips(widget.scores),
+                                )),
                       );
                     },
                     child: Text(
